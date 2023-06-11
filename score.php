063bec326f7191496075577cb8c35300516058e0
Takedacho <122622032+Takedacho@users.noreply.github.com>
2023-06-11 19:17:05 +0900

    php-basic-kadai
    /kadai_005/score.php

---
+++ score.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <?php
+    $total;
+    $total = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+    echo($total / 10);
+    ?>
+</body>
+</html>
